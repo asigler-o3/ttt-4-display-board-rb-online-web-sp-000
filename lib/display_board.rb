72eaccd43b2b8ddfe3d3fab170ad2cf2c29ff77d
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-05 19:12:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,13 +7,7 @@ def display_board (board)
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
-end
-
-def display_board(board)
+ 
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
- puts "   |   |   "
- puts "-----------"
  puts "   | #{board[4]} |   "
- puts "-----------"
- puts "   |   |   "
 end
