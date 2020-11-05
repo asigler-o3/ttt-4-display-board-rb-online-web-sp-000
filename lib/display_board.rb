a02d2d4760406f0ee355281bf79828aa75d2ee44
Ashley Sigler <ashleysigler13@gmail.com>
2020-11-05 19:07:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
-  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
  puts "-----------"
  puts "   |   |   "
- 
+
  puts "   |   |   "
  puts "-----------"
  puts "   | #{board[4]} |   "
